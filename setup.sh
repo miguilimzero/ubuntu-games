68d05637f966237090505d8a4721620d1a2ded36
Dante <35383529+srdante@users.noreply.github.com>
2020-11-02 16:08:58 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,8 +39,8 @@ sudo apt -y install steam
 # CUSTOM PROTON INSTALL
 wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/5.9-GE-8-ST/Proton-5.9-GE-8-ST.tar.gz
 tar -xvzf Proton-5.9-GE-8-ST.tar.gz
-mkdir -p $HOME/.steam/root/compatibilitytools.d/Proton-5.9-GE-8-ST
-mv Proton-5.9-GE-8-ST $HOME/.steam/root/compatibilitytools.d/Proton-5.9-GE-8-ST
+mkdir -p $HOME/.steam/root/compatibilitytools.d
+mv Proton-5.9-GE-8-ST $HOME/.steam/root/compatibilitytools.d
 
 
 # DELETE TMP FILES
