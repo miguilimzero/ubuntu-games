97a14778034f6b77e1c2e4819b03ca1d7d9ae9fc
Dante <35383529+srdante@users.noreply.github.com>
2020-11-03 14:43:57 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 clear
 
+cd $HOME
 mkdir InstallFiles && cd InstallFiles
 
 # GENERAL DRIVES (NVIDIA)
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
-sudo dpkg --add-architecture i386 
 sudo apt -y update
 
-sudo apt -y install libvulkan1 libvulkan1:i386
+wget http://ftp.br.debian.org/debian/pool/main/d/dxvk/dxvk_0.96+ds1-1_all.deb
+wget http://ftp.br.debian.org/debian/pool/main/d/dxvk/dxvk-wine64-development_0.96+ds1-1_amd64.deb
 
+sudo dpkg -i dxvk*
+sudo apt -y install -f
+
+cd /usr/lib/dxvk/wine64-development/ && ./setup_dxvk.sh
+cd $HOME
 
 # WINEHQ INSTALL
+sudo dpkg --add-architecture i386 
 
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
