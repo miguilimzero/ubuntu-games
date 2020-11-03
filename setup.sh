54e0ff1464f0811183d6f9bd7d4842751c384281
Dante <35383529+srdante@users.noreply.github.com>
2020-11-03 14:47:09 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,18 +4,6 @@ clear
 cd $HOME
 mkdir InstallFiles && cd InstallFiles
 
-# GENERAL DRIVES (NVIDIA)
-sudo add-apt-repository -y ppa:graphics-drivers/ppa
-sudo apt -y update
-
-wget http://ftp.br.debian.org/debian/pool/main/d/dxvk/dxvk_0.96+ds1-1_all.deb
-wget http://ftp.br.debian.org/debian/pool/main/d/dxvk/dxvk-wine64-development_0.96+ds1-1_amd64.deb
-
-sudo dpkg -i dxvk*
-sudo apt -y install -f
-
-cd /usr/lib/dxvk/wine64-development/ && ./setup_dxvk.sh
-cd $HOME
 
 # WINEHQ INSTALL
 sudo dpkg --add-architecture i386 
@@ -31,6 +19,20 @@ sudo apt -y install --install-recommends winehq-stable
 
 # WINEHQ DRIVES
 sudo apt install -y winetricks
+
+
+# GENERAL DRIVES (NVIDIA)
+sudo add-apt-repository -y ppa:graphics-drivers/ppa
+sudo apt -y update
+
+wget http://ftp.br.debian.org/debian/pool/main/d/dxvk/dxvk_0.96+ds1-1_all.deb
+wget http://ftp.br.debian.org/debian/pool/main/d/dxvk/dxvk-wine64-development_0.96+ds1-1_amd64.deb
+
+sudo dpkg -i dxvk*
+sudo apt -y install -f
+
+cd /usr/lib/dxvk/wine64-development/ && ./setup_dxvk.sh
+cd $HOME
 
 
 # LUTRIS INSTALL
